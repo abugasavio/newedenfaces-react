806ad4b5c4d0733d87754f106d770d0794256d17
savioabuga <savioabuga@gmail.com>
2016-03-24 11:12:26 +0300

    create add user

--- app/components/app.js
+++ app/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import AddCharacter from './AddCharacter';
+
 
 class App extends React.Component {
     render () {
         return (
           <div>
             <Navbar history={this.props.history} />
-            {this.props.children}
+            <AddCharacter />
+            {this.props.children}   
             <Footer />
           </div>  
             
